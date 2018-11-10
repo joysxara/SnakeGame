fef8fb5c9de1d9ef5b6d333526f234dbb6eb95af
JZ0024 <jessica.zhang123@gmail.com>
2018-11-10 15:29:44 -0500

    Updates on SnakeGame

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,6 +1,9 @@
 
-import pygame
 from pygame.locals import *
+import pygame
+
+picture = pygame.image.load("Assets/grass.jpg")
+picture = pygame.transform.scale(picture, (800,600))
 
 # Create Player class
 class Player:
@@ -27,9 +30,13 @@ class App:
     # Create 800 x 600 grid
     windowWidth = 800
     windowHeight = 600
+    player = 0
 
-    def _init_(self):
+    def __init__(self):
         self._running = True
+        self._display_surf = None
+        self._image_surf = None
+        self.player = Player()
 
     def on_init(self):
         pygame.init()
@@ -37,7 +44,7 @@ class App:
 
         pygame.display.set_caption( ' SNAKE GAME!!!!!!')
         self._running = True
-        self._image_surf = pygame.image.load("Assets/grass.jpg").convert()
+        self._image_surf = picture.convert()
 
     def on_event(self, event):
         if event.type == QUIT:
@@ -48,4 +55,39 @@ class App:
 
     def on_render(self):
         self._display_surf.fill((0,0,0))
-        
+        self._display_surf.blit(self._image_surf, (self.player.x, self.player.y))
+        pygame.display.flip()
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while(self._running):
+            pygame.event.pump()
+            keys = pygame.key.get_pressed()
+
+            if(keys[K_RIGHT]):
+                self.player.moveRight()
+
+            if(keys[K_LEFT]):
+                self.player.moveLeft()
+
+            if(keys[K_UP]):
+                self.player.moveUp()
+
+            if(keys[K_DOWN]):
+                self.player.moveDown()
+
+            if(keys[K_q]):
+                self._running = False
+
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
